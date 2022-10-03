3e2f13c53ef0a76708a4aa16328f8afb2da2e55e
Jose Piedra <j.piedraprog@gmail.com>
2022-10-03 19:00:40 -0400

    feat: added banner and avatar picture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SocialCardComponent } from './components/social-card/social-card.component';
+import { UserBannerComponent } from './components/user-banner/user-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SocialCardComponent,
+    UserBannerComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/user-banner/user-banner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-banner',
+  templateUrl: './user-banner.component.html',
+  styleUrls: ['./user-banner.component.scss']
+})
+export class UserBannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
